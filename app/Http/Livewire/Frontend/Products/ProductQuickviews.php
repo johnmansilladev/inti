3c92030fe9804b62daa4749c9f9c7c8012fcd941
DESKTOP-2D20V62\Support <johnmansillaprada@gmail.com>
2023-01-10 17:39:38 -0500

    cart2

--- app/Http/Livewire/Frontend/Products/ProductQuickviews.php
+++ app/Http/Livewire/Frontend/Products/ProductQuickviews.php
@@ -78,7 +78,9 @@ class ProductQuickviews extends Component
                 'service_name' => $this->service_selected->name,
                 'service_slug' => $this->service_selected->slug,
                 'service_price' => $this->service_selected->pivot->base_price
-            )
+            ),
+            'associatedModel' => $this->product
+
         ];
 
         Cart::add($item);
